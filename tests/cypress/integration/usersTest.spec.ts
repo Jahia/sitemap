b838677562785a4a98750d22dd0aa7d3bc0b2264
Xiang Yu He <xhe@jahia.com>
2021-03-02 15:33:55 -0500

    Remove integration tests that is not part of the sitemap; update package.json to include jahia-cli dependency.

--- tests/cypress/integration/usersTest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { users } from '../page-object/users.page'
-
-describe('navigation to user', () => {
-    it('navigates to the users page successfully', function () {
-        users.goTo()
-    })
-})
