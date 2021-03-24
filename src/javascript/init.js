63e3be2a5f89452a3610e86130b554ac8e04d750
alex <alexandrk_@hotmail.com>
2021-03-24 11:02:53 -0400

    BACKLOG-15102 Update entry registration and dependency

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -1,19 +1,19 @@
 import {registry} from '@jahia/ui-extender';
 import i18next from 'i18next';
 
-// TODO update permission
 export default function () {
+    // TODO update permission
     registry.add('callback', 'sitemap', {
-        targets: ['jahiaApp-init:22'],
+        targets: ['jahiaApp-init:25'],
         callback: async () => {
             await i18next.loadNamespaces('sitemap');
-            registry.add('adminRoute', 'sitemap', {
-                targets: ['jcontent:75'],
-                icon: window.jahia.moonstone.toIconComponent('Search'),
-                label: 'sitemap:labels.seo',
-                isSelectable: false,
-                requiredPermission: 'siteAdminUrlmapping',
-                requireModuleInstalledOnSite: 'sitemap'
+            registry.add('adminRoute', 'siteSettingsSeo/sitemap', {
+                targets: ['jcontent-siteSettingsSeo:75'],
+                label: 'sitemap:labels.sitemap',
+                isSelectable: true,
+                // RequiredPermission: 'siteAdminUrlmapping',
+                requireModuleInstalledOnSite: 'sitemap',
+                render: () => 'Hello Sitemap'
             });
 
             console.log('%c Sitemap registered routes', 'color: #3c8cba');
