a1d982b0c4c871d2a39d8f0fe969c556c90214f9
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-03-05 18:56:16 -0500

    Fixed missing env

--- tests/cypress/plugins/env.js
+++ tests/cypress/plugins/env.js
@@ -1,6 +1,8 @@
 module.exports = (on, config) => {
     console.log('Setting additional environment variables')
     config.env.JAHIA_PROCESSING_URL = process.env.JAHIA_PROCESSING_URL
+    config.env.JAHIA_USERNAME = process.env.JAHIA_USERNAME
+    config.env.JAHIA_PASSWORD = process.env.JAHIA_PASSWORD
 
     console.log('JAHIA_PROCESSING_URL =', config.env.JAHIA_PROCESSING_URL)
     return config
