25124ac008bd2751e9e2062529e7389ad55becac
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-03-05 23:02:00 -0500

    Fixed issue

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,5 +1,19 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const env = require('./env')
 
-module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
+module.exports = (on, config) => {
+    // base jahia cypress plugins
+    require('@jahia/cypress/dist/plugins/registerPlugins').registerPlugins(on, config)
+
+
+    // Load env variables
+    env(on, config)
+
+    const optionsPrinter = {
+        printLogsToConsole: 'always',
+        includeSuccessfulHookLogs: true,
+    }
+    require('cypress-terminal-report/src/installLogsPrinter')(on, optionsPrinter)
+
+    return config
 }
