2e927b5deb332e2075ff8ce2c6314671fb866840
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-03-05 22:09:28 -0500

    Removed code coverage

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -5,9 +5,6 @@ const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
 const env = require('./env')
 
 module.exports = (on, config) => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require("@cypress/code-coverage/task")(on, config);
-
     //https://github.com/archfz/cypress-terminal-report
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('@jahia/cypress/dist/plugins/registerPlugins').registerPlugins(on, config)
