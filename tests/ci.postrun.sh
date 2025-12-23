438c243c460bec8c990c0a6064e9be72b2f0ad7c
Baptiste Grimaud <bgrimaud@jahia.com>
2025-12-23 15:57:17 +0100

    log mariadb general

--- tests/ci.postrun.sh
+++ tests/ci.postrun.sh
@@ -10,6 +10,7 @@ if [[ "${JAHIA_CLUSTER_ENABLED}" == "true" ]]; then
     echo "$(date +'%d %B %Y - %k:%M') [JAHIA_CLUSTER_ENABLED] == Fetching logs from the browsing nodes =="
     docker logs jahia-browsing-a >> ./artifacts/results/jahia-browsing-a.log
     docker logs jahia-browsing-b >> ./artifacts/results/jahia-browsing-b.log
+    docker cp mariadb:/var/lib/mysql/general.log ./artifacts/results/mariadb-general.log
 fi
 
 
