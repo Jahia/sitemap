fd2136c8eb7648938181f442ac63d3788de09225
Baptiste Grimaud <bgrimaud@jahia.com>
2025-12-30 14:27:16 +0100

    update startup.sh to use DOCKER_COMPOSE_FILE env var

--- tests/ci.startup.sh
+++ tests/ci.startup.sh
@@ -1,16 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-source ./set-env.sh
+# TODO for now, use a copy of the one from jahia-cypress.
+# once it's working, create a PR in jahia-cypress to support the DOCKER_COMPOSE_FILE env variable.
 
-echo " == Printing the most important environment variables"
+# This script controls the startup of the container environment
+# It can be used as an alternative to having docker-compose up started by the CI environment
+
+BASEDIR=$(dirname $(readlink -f $0))
+
+source $BASEDIR/set-env.sh
+
+# Set COMPOSE_FILE to DOCKER_COMPOSE_FILE if set, otherwise default to docker-compose.yml
+if [[ -n "${DOCKER_COMPOSE_FILE}" ]]; then
+    COMPOSE_FILE="${DOCKER_COMPOSE_FILE}"
+else
+    COMPOSE_FILE="docker-compose.yml"
+fi
+
+echo " ci.startup.sh == Printing the most important environment variables"
 echo " MANIFEST: ${MANIFEST}"
-echo " NEXUS_USERNAME: ${NEXUS_USERNAME}"
 echo " TESTS_IMAGE: ${TESTS_IMAGE}"
 echo " JAHIA_IMAGE: ${JAHIA_IMAGE}"
 echo " JAHIA_CLUSTER_ENABLED: ${JAHIA_CLUSTER_ENABLED}"
-echo " MODULE_ID: ${MODULE_ID}"
-echo " JAHIA_URL: ${JAHIA_URL}"
+echo " NEXUS_USERNAME: ${NEXUS_USERNAME:0:3}***${NEXUS_USERNAME:(-6)}"
+echo " DOCKER_COMPOSE_FILE: ${DOCKER_COMPOSE_FILE}"
+echo " COMPOSE_FILE: ${COMPOSE_FILE}"
 
-version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
-echo Using @jahia/cypress@$version...
-npx --yes --package @jahia/cypress@$version ci.startup
+echo "$(date +'%d %B %Y - %k:%M') [LICENSE] == Check if license exists in env variable (JAHIA_LICENSE) =="
+if [[ -z ${JAHIA_LICENSE} ]]; then
+    echo "$(date +'%d %B %Y - %k:%M') [LICENSE] == Jahia license does not exist, checking if there is a license file in /tmp/license.xml =="
+    if [[ -f /tmp/license.xml ]]; then
+        echo "$(date +'%d %B %Y - %k:%M') [LICENSE] ==  License found in /tmp/license.xml, base64ing it"
+        export JAHIA_LICENSE=$(base64 -i /tmp/license.xml)
+    else
+        echo "$(date +'%d %B %Y - %k:%M') [LICENSE]  == STARTUP FAILURE, unable to find license =="
+        exit 1
+    fi
+fi
+
+echo "$(date +'%d %B %Y - %k:%M') == Cluster enabled: ${JAHIA_CLUSTER_ENABLED} =="
+if [[ "${JAHIA_CLUSTER_ENABLED}" == "true" ||  ${JAHIA_CLUSTER_ENABLED} == true ]]; then
+    export CLUSTER_PROFILE="--profile cluster"
+fi
+
+echo "$(date +'%d %B %Y - %k:%M') == Starting environment =="
+docker-compose -f "${COMPOSE_FILE}" ${CLUSTER_PROFILE} up -d --renew-anon-volumes
+if [[ "$1" != "notests" ]]; then
+    docker ps -a
+    docker stats --no-stream
+    docker-compose -f "${COMPOSE_FILE}" up --abort-on-container-exit cypress
+fi
