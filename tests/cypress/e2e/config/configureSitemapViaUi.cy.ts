46f534764df19769f020de111918b745ac376020
Baptiste Grimaud <bgrimaud@jahia.com>
2026-01-06 13:06:05 +0100

    fix

--- tests/cypress/e2e/config/configureSitemapViaUi.cy.ts
+++ tests/cypress/e2e/config/configureSitemapViaUi.cy.ts
@@ -4,6 +4,8 @@ import { waitUntilRefresh } from '../../utils/waitUntilRefresh'
 import { removeSitemapConfiguration } from '../../utils/removeSitemapConfiguration'
 import { switchToBrowsingApolloClient, switchToProcessingApolloClient } from '../../utils/apollo'
 import { waitUntilSyncIsComplete } from '../../utils/sync'
+import { enableModule } from '@jahia/cypress'
+import { jahiaProcessingConfig } from '../../utils/serversConfig'
 
 const siteKey = 'digitall'
 const sitePath = `/sites/${siteKey}`
@@ -17,16 +19,20 @@ describe('Testing sitemap configuration via Jahia Admin UI', () => {
     })
 
     before('Verify Sitemap is configured properly for digitall', () => {
+        cy.log('AAA')
+        enableModule('sitemap', siteKey, jahiaProcessingConfig)
+        cy.log('AAA1')
         // Save the root sitemap URL and Flush sitemap cache
         switchToProcessingApolloClient()
+        cy.log('AAA2')
         const siteMapPage = SitemapPage.visit(siteKey, langEn)
+        cy.log('AAA3')
         siteMapPage.inputSitemapRootURL(siteMapRootUrl)
         siteMapPage.clickOnSave()
         siteMapPage.clickTriggerSitemapJob()
-        waitUntilSyncIsComplete()
-        switchToBrowsingApolloClient()
+        cy.log('AAA4')
 
-        cy.apolloProcessing({
+        cy.apollo({
             variables: {
                 pathOrId: sitePath,
                 mixinsFilter: { filters: [{ fieldName: 'name', value: 'jseomix:sitemap' }] },
@@ -40,6 +46,8 @@ describe('Testing sitemap configuration via Jahia Admin UI', () => {
             expect(r.mixinTypes[0].name).to.equal('jseomix:sitemap')
             expect(r.name).to.equal(siteKey)
         })
+        waitUntilSyncIsComplete()
+        switchToBrowsingApolloClient()
     })
 
     // By default, digitall should have some URLs
